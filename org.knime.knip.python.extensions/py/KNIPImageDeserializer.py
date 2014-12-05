0accbf64a1427067248aaa944ddf1ed4f6f4c860
dietzc <christian.dietz@uni-konstanz.de>
2014-12-05 16:52:00 +0100

    refactor NDImage to KNIPImage

--- org.knime.knip.python.extensions/py/KNIPImageDeserializer.py
+++ org.knime.knip.python.extensions/py/KNIPImageDeserializer.py
@@ -1,9 +1,9 @@
 # Deserializing ndarray as tiff stream
 
-import NDImage
+import KNIPImage
 from io import BytesIO
 from PIL import Image
 
 # deserialize TiffFile from ByteStream
 def deserialize(bytes):
-	return NDImage.NDImage(Image.open(BytesIO(bytes)))
+	return KNIPImage.KNIPImage(Image.open(BytesIO(bytes)))
