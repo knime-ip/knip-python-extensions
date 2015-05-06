daaba98e10c11ea73546be4e81a01f7831627cb9
Laurent <abouchar@mpi-cbg.de>
2015-05-06 16:29:57 +0200

    Removed useless import.

--- org.knime.knip.python.extensions/py/KNIPImageSerializer.py
+++ org.knime.knip.python.extensions/py/KNIPImageSerializer.py
@@ -7,7 +7,6 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 import numpy as np
 from tifffile import imsave
 from io import BytesIO
-from fakefile import FakeFile
 
 # Saves NDImage to buffer
 def serialize(knipimage):
