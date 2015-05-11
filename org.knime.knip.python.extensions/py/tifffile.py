46d3780d9273ea500db1af4ca3b15959cf9d4126
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2015-05-11 16:45:31 +0200

    uncommented warning related to issue #3

--- org.knime.knip.python.extensions/py/tifffile.py
+++ org.knime.knip.python.extensions/py/tifffile.py
@@ -149,13 +149,14 @@ from xml.etree import cElementTree as etree
 
 import numpy
 
-try:
-    import _tifffile
-except ImportError:
-    warnings.warn(
-        "failed to import the optional _tifffile C extension module.\n"
-        "Loading of some compressed images will be slow.\n"
-        "Tifffile.c can be obtained at http://www.lfd.uci.edu/~gohlke/")
+# fixes issue #3
+#try:
+#    import _tifffile
+#except ImportError:
+#    warnings.warn(
+#        "failed to import the optional _tifffile C extension module.\n"
+#        "Loading of some compressed images will be slow.\n"
+#        "Tifffile.c can be obtained at http://www.lfd.uci.edu/~gohlke/")
 
 __version__ = '2014.08.24-laurent2015.05.06'
 __docformat__ = 'restructuredtext en'
