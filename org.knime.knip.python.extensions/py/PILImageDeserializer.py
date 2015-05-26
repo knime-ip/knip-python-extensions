28832639718a8410a95ca73738e2af7fb871d4c5
dietzc <christian.dietz@uni-konstanz.de>
2015-05-26 14:59:14 +0200

    remove PNG to PIL and PIL to PNG
    will be moved to KNIME python core as we are not the owner of the PNG
    type

--- org.knime.knip.python.extensions/py/PILImageDeserializer.py
+++
@@ -1,9 +0,0 @@
-# Deserializing ndarray as tiff stream
-
-import KNIPImage
-from io import BytesIO
-from PIL import Image
-
-# deserialize TiffFile from ByteStream
-def deserialize(bytes):
-	return Image.open(BytesIO(bytes))
